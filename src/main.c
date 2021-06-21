69a1d0808a759f4261f1ec1e112ce7aed98360da
Wojciech Lesniak <wojciech.lesniak48@gmail.com>
2021-06-21 21:01:59 +0200

    add function to read file by line

--- src/main.c
+++ src/main.c
@@ -1,36 +1,75 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include <assert.h>
 
 #define MAX_LEN 256
 
+char* getFileByLine(FILE * fp)
+{
+    char chunk[128];
+    size_t len = sizeof(chunk);
+    char *line = malloc(len);
+    if (line != NULL)
+    {
+        line[0] = '\0';
+        while(fgets(chunk, sizeof(chunk), fp) != NULL) 
+        {
+            size_t len_used = strlen(line);
+            size_t chunk_used = strlen(chunk);
+
+            if(len - len_used < chunk_used) 
+            {
+                len *= 2;
+                if ((line = realloc(line, len)) == NULL)
+                {
+                    perror("Unable to reallocate memory for the line buffer.");
+                    free(line);
+                    exit(1);
+                }
+            }
+
+            strncpy(line + len_used, chunk, len - len_used);
+            len_used += chunk_used;
+
+            if(line[len_used - 1] == '\n') 
+            {
+                fputs(line, stdout);
+                // fputs("|*\n", stdout);
+                line[0] = '\0';
+            }
+        }
+    }
+    return 0;
+}
+
+char* getFileByChar(FILE * fp)
+{
+    
+}
+
 int main(int argc, char* argv[argc+1]) 
 {
-    FILE *fp = fopen("/proc/stat", "r");
-    char * buffer = 0;
-    size_t length = 0;
+    // struct stat st;
+    // stat("/proc/stat", &st);
+    // int size = st.st_size;
+    FILE *fp = fopen("/proc/stat", "rb");
     
-    assert(fp);
-    //read line by line
-    fseek64(fp, 0, SEEK_END);
-    length = ftell64(fp);
-    fseek64(fp, 0, SEEK_SET);
-    buffer = malloc(length);
-    if (buffer)
-    {
-        printf("reading\n");
-        size_t tmp = fread64(buffer, 1, 30, fp);
-        printf("%s\n", buffer);
-    }
+    char * buffer = getFileByLine(fp);
+    // char chunk[128];
+    // size_t length = sizeof(chunk);
+    // buffer = malloc(length);
 
-    // while (fgets(buffer, MAX_LEN - 1, fp))
+    // if(fp && buffer)
     // {
-    //     // Remove trailing newline
-    //     buffer[strcspn(buffer, "\n")] = 0;
-    //     printf("%s\n", buffer);
+    //     buffer[0] = '\0';
+    //     while(fgets(chunk, sizeof(chunk), fp) != NULL) 
+    //         {
+    //             fputs(chunk, stdout);
+    //             fputs("|*\n", stdout);  // marker string used to show where the content of the chunk array has ended
+    //         }
+    //     fclose(fp);
     // }
-
     fclose(fp);
+
     return EXIT_SUCCESS;
 }
